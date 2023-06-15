0fd7830c814ed22c6689945058a6311853e0025a
Geziel Carvalho <geziel.natal@gmail.com>
2023-06-15 14:43:37 -0400

    add router

---
+++ Core/Router.php
@@ -0,0 +1,136 @@
+<?php
+namespace Core;
+
+class Router{
+
+    /**
+     * Associative array of routes (the routing table)
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * Parameters from the matched route
+     * @var array
+     */
+    protected $params = [];
+
+        /**
+     * String containig the route extracted from URL
+     * @var array
+     */
+    protected $incomingRoute = '';
+
+    /**
+     * String containig the parameters extracted from URL
+     * @var array
+     */
+    protected $incomingParams = '';
+
+    /**
+     * Parameters from the matched route
+     * @var array
+     */
+    protected $executionPackage = [];
+ 
+    /**
+     * Add a route to the routing table
+     * @param string $route The route URL
+     * @param array  $params Parameters (controller, action, etc)
+     */
+    public function add($route, $params)
+    {
+        $this->routes[] = array('route' => $route, 'params' => $params);
+    }
+
+        /**
+     * Get incoming route
+     * 
+     * @return array  
+     */
+    public function getIncomingRoute()
+    {
+        return $this->incomingRoute;
+    }
+
+    /**
+     * Get all the params from the routing table
+     * 
+     * @return array  
+     */
+    public function getIncomingParams()
+    {
+        return $this->incomingParams;
+    }
+
+    private function validateRoute($incomingRoute, $incomingParams)
+    {
+        $request = $_SERVER['REQUEST_METHOD'];
+        foreach ($this->routes as $route) {
+            if ($route['route'] === $incomingRoute && $route['params']['request'] === $request && !($incomingParams xor isset($route['params']['args']))) {
+                $this->executionPackage = array_merge($route['params'], ['payload' => $incomingParams]);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Match the REST route to the routes in the routing table, setting the $params
+     * property if a route is found
+     * 
+     * @param string $url The route URL
+     * 
+     * @return boolean true if a match found, false otherwise
+     */
+    public function match ($query_string_received)
+    {
+        $reg_exp = "/[&=?]/";
+
+        $query_parts = explode('/', $query_string_received);
+
+        foreach ($query_parts as $part) {
+            if (preg_match($reg_exp, $part)) {
+                $this->incomingParams = rtrim($part, "/");
+            } else {
+                $this->incomingRoute .= '/' . rtrim($part, "/");
+            }
+        }
+
+        if ($this->validateRoute($this->incomingRoute, $this->incomingParams)) {
+            return true;
+        }
+
+        $this->incomingRoute = '/' . implode( '/',array_slice($query_parts, 0, sizeof($query_parts) - 1));
+        $this->incomingParams = $query_parts[sizeof($query_parts) - 1];
+
+        if ($this->validateRoute($this->incomingRoute, $this->incomingParams)) {
+            return true;
+        }
+
+        return false; 
+    }
+
+    /**
+     * Dispatches the route by calling the appropriate controller method
+     */
+    public function dispatch()
+    {
+        $controller = $this->executionPackage['controller'];
+        $action = $this->executionPackage['action'];
+        $id = $this->executionPackage['payload'] ?? null;
+
+        if (class_exists($controller)) {
+            $controller_object = new $controller($this->executionPackage);
+            if (is_callable([$controller_object, $action])) {
+                $controller_object->$action($id);
+            } else {
+                http_response_code(404);
+                throw new \Exception("Method $action (in controller $controller) not found");
+            }
+        } else {
+            http_response_code(404);
+            throw new \Exception("Controller class $controller not found");
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,15 +1,36 @@
 <?php
-header('Content-Type: application/json');
-http_response_code(200);
-echo json_encode([
-    'success' => true,
-    'status' => 200,
-    'message' => 'Hello World',
-    'data' => [
-        'user'=> [
-            'name' => 'John Doe',
-            'age' => 25,
-            'address' => 'New York'
-        ]
-    ]
-]);
+use Core\Router;
+
+spl_autoload_register(function ($class) {
+    $root = dirname(__DIR__);
+    $file = $root . "/" . str_replace("\\", '/', $class) . ".php";
+    if (is_readable($file)) {
+        require $root . "/" . str_replace("\\", "/", $class) . ".php";
+    }
+});
+
+/**
+ * Routing 
+ */
+$router = new Router();
+
+// Add the routes
+$router->add('/', ['controller' => 'Home', 'action' => 'index', 'request' => 'GET']); // METHOD GET
+$router->add('/users', ['controller' => 'App\Controllers\UserController', 'action' => 'index', 'request' => 'GET']); // METHOD GET
+$router->add('/users', ['controller' => 'App\Controllers\UserController', 'action' => 'show', 'request' => 'GET', 'args' => ['id']]); // METHOD GET
+$router->add('/users', ['controller' => 'App\Controllers\UserController', 'action' => 'store', 'request' => 'POST']); // METHOD POST
+$router->add('/users', ['controller' => 'App\Controllers\UserController', 'action' => 'update', 'request' => 'PUT']); // METHOD PUT
+$router->add('/users', ['controller' => 'App\Controllers\UserController', 'action' => 'destroy', 'request' => 'DELETE']); // METHOD DELETE
+
+
+// Get route from external URL
+$url = $_SERVER['QUERY_STRING'];
+
+// Check if route exists
+if ($router->match($url)) {
+    $router->dispatch(); 
+} else {
+    header("Content-Type: application/json");
+    http_response_code(404);
+    echo json_encode(["error" => "No route found from URL $url"]);
+}
