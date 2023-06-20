048d5f4afd690116089ca3afedf22267ba5235f4
Geziel Carvalho <geziel.natal@gmail.com>
2023-06-19 22:23:49 -0400

    hash password before saving

--- Core/View.php
+++ Core/View.php
@@ -8,7 +8,7 @@ class View
     {
         extract($args, EXTR_SKIP);
 
-        $file = "../App/Views/$view";
+        $file = "../app/views/$view";
 
         if (is_readable($file)) {
             require $file;
